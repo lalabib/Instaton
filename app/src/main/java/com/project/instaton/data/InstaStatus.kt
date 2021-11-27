4f9e1534aabd0be45c93a727a81bc6797db2ff42
Labib Hiba <57593172+lalabib@users.noreply.github.com>
2021-11-28 00:05:11 +0700

    Update InstaStatus.kt

--- app/src/main/java/com/project/instaton/data/InstaStatus.kt
+++ app/src/main/java/com/project/instaton/data/InstaStatus.kt
@@ -1,3 +1,7 @@
 package com.project.instaton.data
 
-data class InstaStatus(var id: Int, var name: String, var picture: String)
+data class InstaStatus(
+  var id: Int,       
+  var name: String,       
+  var picture: String
+)
